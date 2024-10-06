322d905f6521e27f323981ccd8390d2ce8ca1e68
lisheng <lisheng226735@163.com>
2024-10-06 18:21:37 +0800

    commit from vscode

--- a.cpp
+++ a.cpp
@@ -1,7 +1,17 @@
+/*
+ * @Descripttion:
+ * @Author: laidui
+ * @version:
+ * @Date: 2024-10-06 12:03:47
+ * @LastEditors: lai dui
+ * @LastEditTime: 2024-10-06 18:19:41
+ */
 #include <bits/stdc++.h>
 
 using namespace std;
-int main(){
-	int a = b;	
+int main()
+{
+	int a, b;
+	cin >> a >> b;
 	return 0;
 }
