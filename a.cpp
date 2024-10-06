28befa560c10b41378921226065c8017c9b26fee
lisheng <lisheng226735@163.com>
2024-10-06 20:23:09 +0800

    commit a.cpp

--- a.cpp
+++ a.cpp
@@ -1,7 +1,16 @@
+/*
+ * @Descripttion:
+ * @Author: laidui
+ * @version:
+ * @Date: 2024-10-06 12:03:47
+ * @LastEditors: lai dui
+ * @LastEditTime: 2024-10-06 18:35:22
+ */
 #include <bits/stdc++.h>
 
 using namespace std;
-int main(){
-	int a = b;	
+int main()
+{
+	int a = 0;
 	return 0;
 }
