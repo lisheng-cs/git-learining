10aee62f1681076046c9b98896ac4fd3f9f4b784
lisheng <lisheng226735@163.com>
2024-10-07 15:06:13 +0800

    commit from Clion

---
+++ test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 李胜 on 2024-10-07.
+//
+
+#include "test.h"
---
+++ test.h
@@ -0,0 +1,14 @@
+//
+// Created by 李胜 on 2024-10-07.
+//
+
+#ifndef GIT_LEARINING_TEST_H
+#define GIT_LEARINING_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //GIT_LEARINING_TEST_H
